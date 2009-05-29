57a3defbce627595c4f5f64ff5f28c67bde55ec3
Gustaf Mossakowski <web@koenige.org>
2009-05-29 11:04:00 +0000

    update in auth core module, corrected wrong paths
    git-svn-id: file:///Repos/zzwrap/trunk@16 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- www/_lib/cmscore/auth.inc.php
+++ www/_lib/cmscore/auth.inc.php
@@ -1,142 +1,76 @@
-<?php 
+<?php
 
 // Zugzwang CMS
 // (c) Gustaf Mossakowski, <gustaf@koenige.org> 2007
-// CMS Authentifizierungsfunktionen und -aktivierung
+// Authentification
 
-
-// Activate authentification mechanism
-// (c) Gustaf Mossakowski, gustaf@koenige.org, 2007
-if (!empty($zz_setting['auth_urls'])) {
-	$url = parse_url($_SERVER['REQUEST_URI']);
-	foreach($zz_setting['auth_urls'] as $auth_url) {
-		if (substr($url['path'], 0, strlen($auth_url)) == $auth_url
-			&& $url['path'] != $zz_setting['login_url']) {
-			require_once $zz_setting['scripts'].'/inc/auth.inc.php';
-		}
-	}
-}
 
 // Local modifications to SQL queries
-require_once $zz_setting['modules'].'/local-sql-auth.inc.php';
+require $zz_setting['inc_local'].'/cms-sql-auth.inc.php';
 
-// Logout from restricted area
-// (c) Gustaf Mossakowski, gustaf@koenige.org, 2007
-function cms_logout($variablen) {
-	global $zz_setting;
-	global $zz_conf;
-	global $zz_sql;
-	
-	if (empty($_SESSION)) session_start();				// start session
-	if (!empty($_SESSION['login_id']))					// update login db if logged in, set to logged out
-		$sql = sprintf($zz_sql['logout'], $_SESSION['login_id']);
-	session_destroy();
-	if (!empty($sql)) $result = mysql_query($sql);
+// Set variables
+global $zz_setting;
+global $zz_page;
+$now = time();
 
+// start PHP session
+if (empty($_SESSION)) session_start();
+
+// if it's not local access (e. g. on development server), all access should go via secure connection
+$zz_setting['protocol'] = 'http'.((!empty($zz_setting['no_https']) OR $zz_setting['local_access']) ? '' : 's');
+// calculate maximum login time
+// you'll stay logged in for x minutes
+$keep_alive = $zz_setting['logout_inactive_after'] * 60;
+
+// Falls nicht oder zu lange eingeloggt, auf Login-Seite umlenken
+$qs['request'] = false; // initialize request, should be in front of nocookie
+if (empty($_SESSION['logged_in']) OR 
+	$now > ($_SESSION['last_click_at'] + $keep_alive)) {
+	if (!empty($zz_page['url']['full']['query'])) {
+		// parse URL for no-cookie to hand it over to login.inc
+		// in case cookies are not allowed
+		parse_str($zz_page['url']['full']['query'], $query_string);
+		if (isset($query_string['no-cookie'])) {
+			$qs['nocookie'] = 'no-cookie'; // add no-cookie to query string so login knows that there's no cookie (in case SESSIONs don't work here)
+			unset($query_string['no-cookie']);
+		}
+		$full_query_string = '';
+		// glue query string back together
+		if ($query_string)
+			foreach($query_string as $key => $value) {
+				if (is_array($value)) {
+					foreach ($value as $subkey => $subvalue)
+						$full_query_string[] = $key.'['.$subkey.']='.urlencode($subvalue);
+				} else {
+					$full_query_string[] = $key.'='.urlencode($value);
+				}
+			}
+		if ($full_query_string) {
+			$query_string = '?'.implode('&', $full_query_string);
+		} else
+			$query_string = '';
+	} else $query_string = '';
+	$request = $zz_page['url']['full']['path'].$query_string;
+	// do not unneccessarily expose URL structure
+	if ($request == $zz_setting['login_entryurl']) unset($qs['request']); 
+	else $qs['request'] = 'url='.urlencode($request);
 	header('Location: '.$zz_setting['protocol'].'://'.$zz_setting['hostname']
-		.$zz_setting['login_url'].'?logout=true');
+		.$zz_setting['login_url']
+		.(count($qs) ? '?'.implode('&', $qs) : ''));
 	exit;
 }
 
-// Login from restricted area
-// (c) Gustaf Mossakowski, gustaf@koenige.org, 2007
-function cms_login($variablen) {
-	global $zz_setting;
-	global $zz_conf;
-	global $zz_sql;
+// start database connection
+require_once $zz_setting['db_inc'];
 
-	$myurl = parse_url($_SERVER['REQUEST_URI']);
-	if (!empty($myurl['query']) && substr($myurl['query'], 0, 4) == 'url=')
-		$url = substr(rawurldecode($myurl['query']), 4);
-	else
-		$url = $zz_setting['login_entryurl'];
-	$msg = false;
-
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		if (empty($_SESSION)) session_start();
-		if (!empty($_POST['username']))
-			$login_username = $_POST['username']; // todo: addslashes, get_magix_quotes
-		if (!empty($_POST['password']))
-			$password = $_POST['password'];
-
-		$zz_setting['hostname'] = $_SERVER['HTTP_HOST'];
-		if (empty($zz_setting['no_https']))
-			$zz_setting['local_access'] 
-				= (substr($zz_setting['hostname'], -6) == '.local' ? true : false);
-		else
-			$zz_setting['local_access'] = true;
-		$zz_setting['protocol'] = 'http'.($zz_setting['local_access'] ? '' : 's');
-		
-		$zz_setting['myhost'] = $zz_setting['protocol'].'://'.$zz_setting['hostname'];
-
-	// Benutzername und Passwort werden ueberprueft
-		$sql = sprintf($zz_sql['login'], mysql_real_escape_string($login_username));
-		$result = mysql_query($sql);
-		$zz_conf['debug'] = true;
-		if ($zz_conf['debug']) if (mysql_error()) {
-			echo mysql_error();
-			echo '<br>'.$sql;
-		}
-		$_SESSION['logged_in'] = false;
-		if ($result) {
-			if (mysql_num_rows($result) == 1) {
-				$data = mysql_fetch_assoc($result);
-				if (mysql_result($result, 0, 0) == md5($password)) {
-					$_SESSION['logged_in'] = true;
-				}
-			}
-		} else {
-			require $zz_setting['http_errors'].'/503.php';
-			exit;
-		}
-		
-		if (!$_SESSION['logged_in']) { // Benutzername oder Passwort falsch
-			$msg = 'Passwort oder Benutzername falsch. Bitte versuchen Sie es erneut.';
-		} else {
-			$_SESSION['letzter_klick_um'] = time();
-			$i = 0;
-			foreach ($data as $key => $value) {
-				if ($i) $_SESSION[$key] = $value; 
-				$i++;
-			}
-			// Weiterleitung zur geschuetzten Startseite
-			if ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1')
-				if (php_sapi_name() == 'cgi')
-					header('Status: 303 See Other');
-				else
-					header('HTTP/1.1 303 See Other');
-			header('Location: '.$zz_setting['myhost'].$url);
-			exit;
-		}
-	}
-	
-	$page['text'] = '<div id="login">';
-	if (isset($myurl['query']) && $myurl['query'] == 'logout') {
-		if (empty($_SESSION)) session_start();
-		if (!empty($_SESSION['login_id']))
-			$sql = sprintf($zz_sql['logout'], $_SESSION['login_id']);
-		session_destroy();
-		if (!empty($sql)) $result = mysql_query($sql);
-		$page['text'] .= '<p><strong class="error">Sie haben sich abgemeldet.</strong></p>';
-	}
-
-	$page['text'] .='
-<p>F&uuml;r den Zugang zum internen Bereich ist eine Anmeldung erforderlich. Bitte geben Sie unten den von uns erhaltenen Benutzernamen mit Pa&szlig;wort ein.</p>
-<p>Damit der Login funktioniert, m&uuml;ssen Sie nach &Uuml;bermittlung der Anmeldedaten einen Cookie akzeptieren. Nach zehn Minuten Inaktivit&auml;t werden Sie aus Sicherheitsgr&uuml;nden automatisch wieder abgemeldet!</p>
-
-<form action="./';
-	if (isset($url)) $page['text'].= '?url='.urlencode($url); 
-	$page['text'].= '" method="POST" class="login">
-<fieldset><legend>Login</legend>
-<p><label for="username"><strong>Benutzername:</strong></label> <input type="text" name="username" id="username"></p>
-<p><label for="password"><strong>Passwort:</strong></label> <input type="password" name="password" id="password"></p>
-<p class="submit"><input type="submit" value="Anmelden"></p>
-</fieldset>
-';
-	if ($msg) $page['text'].= '<p class="error">'.$msg.'</p>';
-
-	$page['text'].= '</form></div>';
-	return $page;
+// save successful request in database to prolong login time
+$_SESSION['last_click_at'] = $now;
+if (!empty($_SESSION['login_id'])) {
+	$sql = sprintf($zz_sql['last_click'], $now, $_SESSION['login_id']);
+	$result = mysql_query($sql);
+	// it's not important if an error occurs here
+	if (!$result)
+		zz_errorhandling(sprintf(cms_text('Could not save "last_click" in database.')."\n\n%s\n%s", mysql_error(), $sql), E_USER_NOTICE);
 }
 
 ?>
