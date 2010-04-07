074a179b5195022e1016f1065a2bbb7d616a20e0
Gustaf Mossakowski <web@koenige.org>
2010-04-07 09:31:20 +0000

    296 errors.php-Skript angepaßt, bindet nun auch die Datenbankverbindung an derselben Stelle wie zzwrap.php ein
    git-svn-id: file:///Repos/zzwrap/trunk@32 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- www/errors.php
+++ www/errors.php
@@ -12,15 +12,17 @@ global $zz_setting;
 // basic zzwrap files
 if (empty($zz_setting['inc'])) require_once 'paths.inc.php';
 if (file_exists($zz_setting['inc'].'/config.inc.php'))
-	require_once $zz_setting['inc'].'/config.inc.php'; 		// configuration
-require_once $zz_setting['core'].'/defaults.inc.php'; // default configuration
+	require_once $zz_setting['inc'].'/config.inc.php'; 	// configuration
+require_once $zz_setting['core'].'/defaults.inc.php';	// set default variables
 require_once $zz_setting['core'].'/errorhandling.inc.php';	// CMS errorhandling
+require_once $zz_setting['db_inc']; // Establish database connection
 require_once $zz_setting['core'].'/language.inc.php';	// include language settings
-require_once $zz_setting['core'].'/core.inc.php';	// CMS Kern
-require_once $zz_setting['core'].'/page.inc.php';	// CMS Seitenskripte
+require_once $zz_setting['core'].'/core.inc.php';	// CMS core scripts
+require_once $zz_setting['core'].'/page.inc.php';	// CMS page scripts
 
-// establish database connection
-require_once $zz_setting['db_inc'];
+if (!empty($zz_setting['mod_rewrite_error']))
+	if (preg_match('/[a-zA-Z0-9]/', substr($_SERVER['REQUEST_URI'], 1, 1)))
+		wrap_error('mod_rewrite does not work as expected: '.$_SERVER['REQUEST_URI'].' ('.$_SERVER['SCRIPT_NAME'].')', E_USER_NOTICE);
 
 // -- 1. check what kind of error page it is
 
@@ -50,10 +52,15 @@ foreach ($codes_from_file as $line) {
 }
 
 if (!empty($_GET['code']) AND in_array($_GET['code'], array_keys($codes)))
-	$page['code'] = $_GET['code'];
-elseif (empty($page['code']) OR !in_array($page['code'], array_keys($codes)))
-	$page['code'] = 404;
-$error_messages = $codes[$page['code']];
+	$page['status'] = $_GET['code'];
+elseif (empty($page['status']) OR !in_array($page['status'], array_keys($codes)))
+	$page['status'] = 404;
+if ($page['status'] == 404 AND substr($_SERVER['REQUEST_URI'], 0, 7) == 'http://') {
+	// probably badly designed robot, away with it
+	$page['status'] = 400;
+}
+
+$error_messages = $codes[$page['status']];
 $extra_description_codes = array(403, 404, 410);
 
 // -- 2. set page elements
@@ -63,11 +70,12 @@ $page['last_update'] = false;
 $page['breadcrumbs'] = '<strong><a href="'.$zz_setting['homepage_url'].'">'
 	.$zz_conf['project'].'</a></strong> '.$zz_page['breadcrumbs_separator'].' '
 	.wrap_text($error_messages['title']); 
-$page['pagetitle'] = strip_tags($page['code'].' '.wrap_text($error_messages['title']).' ('.$zz_conf['project'].')'); 
+$page['pagetitle'] = strip_tags($page['status'].' '.wrap_text($error_messages['title']).' ('.$zz_conf['project'].')'); 
 $page['h1'] = wrap_text($error_messages['title']);
 $page['error_description'] = sprintf(wrap_text($error_messages['description']), $_SERVER['REQUEST_METHOD']);
-if (in_array($page['code'], $extra_description_codes)) {
-	$page['error_explanation'] = sprintf(wrap_text('Please try to find the content you were looking for from our <a href="%s">main page</a>.'), $zz_setting['homepage_url']);
+if (in_array($page['status'], $extra_description_codes)) {
+	$page['error_explanation'] = sprintf(wrap_text('Please try to find the content you were looking for from our <a href="%s">main page</a>.'),
+		$zz_setting['homepage_url']);
 } else {
 	$page['error_explanation'] = '';
 }
@@ -90,7 +98,7 @@ if ($zz_setting['brick_page_templates'] == true) {
 	echo wrap_htmlout_page($page);
 } else {
 	$page['output'] = str_replace('%%% page h1 %%%', $page['h1'], $page['output']);
-	$page['output'] = str_replace('%%% page code %%%', $page['code'], $page['output']);
+	$page['output'] = str_replace('%%% page code %%%', $page['status'], $page['output']);
 	$page['output'] = str_replace('%%% page error_description %%%', $page['error_description'], $page['output']);
 	$page['output'] = str_replace('%%% page error_explanation %%%', $page['error_explanation'], $page['output']);
 	include $zz_page['head'];
@@ -100,7 +108,7 @@ if ($zz_setting['brick_page_templates'] == true) {
 
 // -- 5. error logging
 
-if ($page['code'] == 404
+if ($page['status'] == 404
 	AND isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_USER_AGENT'])) {
 	$requested = $zz_page['url']['full']['scheme'].'://'
 		.$zz_page['url']['full']['host'].$zz_page['url']['full']['path'];
@@ -110,36 +118,34 @@ if ($page['code'] == 404
 		&& substr($_SERVER['REQUEST_URI'], -1) != '&'		// encoded mail addresses, some bots are too stupid for them
 		&& substr($_SERVER['REQUEST_URI'], -3) != '%26'		// encoded mail addresses, some bots are too stupid for them
 	) {
-		$msg = sprintf(wrap_text("The URL\n\n <%s> was requested from\n\n <%s>\n\n with the IP address %s\n (Browser %s)\n\n but could not be found on the server"), 
+		$msg = sprintf(wrap_text("The URL\n\n%s was requested from\n\n%s\n\n with the IP address %s\n (Browser %s)\n\n but could not be found on the server"), 
 			$requested, $_SERVER['HTTP_REFERER'], $_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT']);
 		if (!empty($zz_conf['user'])) $msg .= "\n\n".wrap_text('User').': '.$zz_conf['user'];
 		elseif (!empty($_SESSION['username'])) $msg .= "\n\n".wrap_text('User').': '.$_SESSION['username'];
-		zz_errorhandling($msg, E_USER_WARNING);
+		wrap_error($msg, E_USER_WARNING);
 	}
-}
-
-// 503
-if ($page['code'] == 503 AND !empty($sql)) {
+} elseif ($page['status'] == 503 AND !empty($sql)) {
 	if (!empty($zz_conf['error_handling']) AND $zz_conf['error_handling'] == 'mail') {
-		$mailtext = false;
+		$msg = false;
 		if (!empty($sql)) 
-			$mailtext = "Database error:\n\n".mysql_error()."\n\nSQL: ".$sql;
-		$mailtext = sprintf($text['The following error(s) occured in project %s:'], 
-			$zz_conf['project'])."\n\n".$mailtext;
-		$mailtext .= "\n\n-- \nURL: http://".$_SERVER['SERVER_NAME']
+			$msg = "Database error:\n\n".mysql_error()."\n\nSQL: ".$sql;
+		$msg = sprintf(wrap_text('The following error(s) occured in project %s:'), 
+			$zz_conf['project'])."\n\n".$msg;
+		$msg .= "\n\n-- \nURL: http://".$_SERVER['SERVER_NAME']
 			.$_SERVER['REQUEST_URI']
 			."\nIP: ".$_SERVER['REMOTE_ADDR']
-			."\nBrowser: ".$_SERVER['HTTP_USER_AGENT'];		
-		if ($zz_conf['user'])
-			$mailtext .= "\n".wrap_text('User').': '.$zz_conf['user'];
+			."\nBrowser: ".(!empty($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : wrap_text('unknown'));		
+		if (!empty($zz_conf['user']))
+			$msg .= "\n".wrap_text('User').': '.$zz_conf['user'];
 		elseif (!empty($_SESSION['username'])) 
-			$mailtext .= "\n".wrap_text('User').': '.$_SESSION['username'];
-		mail ($zz_conf['error_mail_to'], '['.$zz_conf['project'].'] '
-			.$text['Error during database operation'], 
-			$mailtext, 'MIME-Version: 1.0
-Content-Type: text/plain; charset='.$zz_conf['charset'].'
+			$msg .= "\n".wrap_text('User').': '.$_SESSION['username'];
+		wrap_error($msg, E_USER_ERROR);
+/*		mail ($zz_conf['error_mail_to'], '['.$zz_conf['project'].'] '
+			.wrap_text('Error during database operation'), 
+			$msg, 'MIME-Version: 1.0
+Content-Type: text/plain; charset='.$zz_conf['character_set'].'
 Content-Transfer-Encoding: 8bit
-From: '.$zz_conf['error_mail_from']);
+From: '.$zz_conf['error_mail_from']);*/
 	// TODO: check what happens with utf8 mails
 	}
 }
