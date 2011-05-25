9dabaa0e2ef6f613e27a2d5fe427025e0e561973
Gustaf Mossakowski <web@koenige.org>
2011-05-25 11:21:37 +0000

    bugfix wrap_cache_filename(), base muß auch urlencoded werden
    git-svn-id: file:///Repos/zzwrap/trunk@121 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/core.inc.php
+++ zzwrap/core.inc.php
@@ -1232,9 +1232,10 @@ function wrap_cache_filename($type = 'url') {
 	if ($type === 'domain') return $file;
 
 	$base = $zz_setting['base'];
+	if (substr($base, 0, 1) == '/') $base = substr($base, 1);
 	if (substr($base, -1) != '/') $base .= '/';
 	if (!empty($my['query'])) $my['path'] .= '?'.$my['query'];
-	$file .= $base.urlencode($my['path']);
+	$file .= '/'.urlencode($base.$my['path']);
 	if ($type === 'url') return $file;
 
 	$file .= '.headers';
