7597b9fbe1b87952c8ac19e476dfb35e8432126e
Gustaf Mossakowski <web@koenige.org>
2011-07-26 12:45:40 +0000

    Auskommentiertes altes Error-Skript gelöscht
    git-svn-id: file:///Repos/zzwrap/trunk@188 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- www/errors.php
+++ www/errors.php
@@ -10,23 +10,4 @@ require_once $zz_setting['inc'].'/library/zzwrap/zzwrap.php';
 
 zzwrap();
 
-/*
-// in case config has already been included
-global $zz_page;
-global $zz_setting;	
-
-// basic zzwrap files
-if (empty($zz_setting['inc'])) require_once 'paths.inc.php';
-if (file_exists($zz_setting['inc'].'/config.inc.php'))
-	require_once $zz_setting['inc'].'/config.inc.php'; 	// configuration
-require_once $zz_setting['core'].'/defaults.inc.php';	// set default variables
-require_once $zz_setting['core'].'/errorhandling.inc.php';	// CMS errorhandling
-require_once $zz_setting['db_inc']; // Establish database connection
-require_once $zz_setting['core'].'/core.inc.php';	// CMS core scripts
-
-if (!isset($page)) $page = array();
-wrap_errorpage($page, $zz_page);
-exit;
-*/
-
 ?>
