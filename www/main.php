00e85e4c15c9fd861183c73d6920ba8ac075ae75
Gustaf Mossakowski <web@koenige.org>
2012-05-10 17:40:14 +0000

    SiB: _scripts/path und main zusammengefasst, wie in zzproject
    git-svn-id: file:///Repos/zzwrap/trunk@269 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- www/main.php
+++ www/main.php
@@ -1,13 +1,22 @@
 <?php 
 
 // zzwrap (Zugzwang Project)
-// (c) Gustaf Mossakowski, <gustaf@koenige.org> 2007-2011
+// (c) Gustaf Mossakowski, <gustaf@koenige.org> 2007-2012
 // Main script
 
 
-require_once 'paths.inc.php';
-require_once $zz_setting['inc'].'/library/zzwrap/zzwrap.php';
+// root directory
+// some providers are not able to configure the root directory correctly
+// then you'll have to correct that here
+$zz_conf['root'] = $_SERVER['DOCUMENT_ROOT'];
 
+// scripts library
+// if your provider does not support putting the include scripts below
+// document root, change accordingly
+$zz_setting['inc'] = $zz_conf['root'].'/../_inc';
+
+// CMS will be started
+require_once $zz_setting['inc'].'/library/zzwrap/zzwrap.php';
 zzwrap();
 
 ?>
