63ea53142f8b9cce170c46dd1c30dd7f513c9dfa
Gustaf Mossakowski <web@koenige.org>
2012-08-20 17:52:04 +0000

    zzwrap: wrap_db_connect() nutzt jetzt mysql_set_charset() statt SET NAMES, da sonst mysql_real_escape_string() nicht richtig funktioniert
    git-svn-id: file:///Repos/zzwrap/trunk@379 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/database.inc.php
+++ zzwrap/database.inc.php
@@ -92,7 +92,7 @@ function wrap_db_connect() {
 	} else {
 		$charset = $zz_setting['encoding_to_mysql_encoding'][$zz_conf['character_set']];
 	}
-	if ($charset) mysql_set_charset($character_set);
+	if ($charset) mysql_set_charset($charset);
 	return true;
 }
 
