d9352323c4c6d9e1c38b4ba60b9b9932101bfd6e
Gustaf Mossakowski <web@koenige.org>
2015-08-04 12:11:46 +0000

    zzwrap database: add support for three list-values for wrap_db_fetch()
    git-svn-id: file:///Repos/zzwrap/trunk@637 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/database.inc.php
+++ zzwrap/database.inc.php
@@ -239,9 +239,19 @@ function wrap_db_fetch($sql, $id_field_name = false, $format = false, $errorcode
 		} elseif (substr($format, 0, 5) === 'list ') {
 			$listkey = substr($format, 5);
 			$listkey = explode(' ', $listkey);
-			while ($line = mysql_fetch_assoc($result)) {
-				$lines[$line[$id_field_name[0]]][$listkey[0]] = $line[$id_field_name[0]];
-				$lines[$line[$id_field_name[0]]][$listkey[1]][$line[$id_field_name[1]]] = $line;
+			if (count($listkey) === 2) {
+				while ($line = mysql_fetch_assoc($result)) {
+					$lines[$line[$id_field_name[0]]][$listkey[0]] = $line[$id_field_name[0]];
+					$lines[$line[$id_field_name[0]]][$listkey[1]][$line[$id_field_name[1]]] = $line;
+				}
+			} else {
+				while ($line = mysql_fetch_assoc($result)) {
+					$lines[$line[$id_field_name[0]]][$listkey[0]] = $line[$id_field_name[0]];
+					if (!isset($lines[$line[$id_field_name[0]]][$listkey[1]][$line[$id_field_name[1]]])) {
+						$lines[$line[$id_field_name[0]]][$listkey[1]][$line[$id_field_name[1]]] = $line;
+					}
+					$lines[$line[$id_field_name[0]]][$listkey[1]][$line[$id_field_name[1]]][$listkey[2]][$line[$id_field_name[2]]] = $line;
+				}
 			}
 		} else {
 			// default or unknown format
