2d2acd4a95fe553865cb19068bc7e1556e2deeab
Gustaf Mossakowski <web@koenige.org>
2016-03-09 15:46:09 +0000

    zzwrap database: update wrap_edit_sql(), bugfix
    git-svn-id: file:///Repos/zzwrap/trunk@745 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/database.inc.php
+++ zzwrap/database.inc.php
@@ -709,14 +709,14 @@ function wrap_edit_sql($sql, $n_part = false, $values = false, $mode = 'add') {
 		}
 	}
 	if ($mode === 'list') {
-		if (!isset($tables)) return zz_return(array());
+		if (!isset($tables)) return array();
 		foreach (array_keys($tables) as $index) {
 			$tables[$index] = trim($tables[$index]);
 			if (strstr($tables[$index], ' ')) {
 				$tables[$index] = trim(substr($tables[$index], 0, strpos($tables[$index], ' ')));
 			}
 		}
-		return zz_return($tables);
+		return $tables;
 	}
 	$statements_asc = array_reverse($statements_desc);
 	foreach ($statements_asc as $statement) {
