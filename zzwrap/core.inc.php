63749d51fb716252adafd60877326fa036450f3d
Gustaf Mossakowski <gustaf@koenige.org>
2019-12-30 09:22:21 +0000

    core: check only for HTML output if canonical ending is correct
    git-svn-id: file:///Repos/zzwrap/trunk@1164 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/core.inc.php
+++ zzwrap/core.inc.php
@@ -421,7 +421,8 @@ function wrap_check_canonical($zz_page, $page) {
 	}
 	
 	// if database allows field 'ending', check if the URL is canonical
-	if (!empty($zz_page['db'][wrap_sql('ending')])) {
+	// just for HTML output!
+	if (!empty($zz_page['db'][wrap_sql('ending')]) AND $page['content_type'] === 'html') {
 		$ending = $zz_page['db'][wrap_sql('ending')];
 		// if brick_format() returns a page ending, use this
 		if (isset($page['url_ending'])) $ending = $page['url_ending'];
