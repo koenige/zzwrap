2b56add67ab03e6b676b41f196703bd591cfd967
Gustaf Mossakowski <gustaf@koenige.org>
2020-02-01 19:49:58 +0000

    core: set page ending to none only for non-HTML ressources with asterisk ending in webpages table
    git-svn-id: file:///Repos/zzwrap/trunk@1180 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/core.inc.php
+++ zzwrap/core.inc.php
@@ -424,7 +424,7 @@ function wrap_check_canonical($zz_page, $page) {
 	// if database allows field 'ending', check if the URL is canonical
 	// just for HTML output!
 	if (!empty($page['content_type']) AND $page['content_type'] !== 'html'
-		AND in_array($_SERVER['REQUEST_METHOD'], ['HEAD', 'GET'])) {
+		AND substr($zz_page['db']['identifier'], -1) === '*') {
 		if (empty($page['url_ending'])) $page['url_ending'] = 'none';
 	}
 	if (!empty($zz_page['db'][wrap_sql('ending')])) {
