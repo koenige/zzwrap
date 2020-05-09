1cc5fa0458928f33a68b8d4048c3bd9b264df5fb
Gustaf Mossakowski <gustaf@koenige.org>
2020-05-09 09:40:54 +0000

    zzwrap: add 'strip_tags' to brick formatting functions
    git-svn-id: file:///Repos/zzwrap/trunk@1216 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/zzwrap.php
+++ zzwrap/zzwrap.php
@@ -320,7 +320,7 @@ function wrap_set_defaults_pre_conf() {
 		'markdown', 'wrap_date', 'rawurlencode', 'wordwrap', 'nl2br',
 		'htmlspecialchars', 'wrap_html_escape', 'wrap_latitude',
 		'wrap_longitude', 'wrap_number', 'ucfirst', 'wrap_time', 'wrap_bytes',
-		'wrap_duration'
+		'wrap_duration', 'strip_tags'
 	];
 
 	if (!$zz_setting['local_access']) {
