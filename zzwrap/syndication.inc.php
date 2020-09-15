b0d6c15ac2125cac3eec761b202fc1e8facdc510
Gustaf Mossakowski <gustaf@koenige.org>
2020-09-15 10:37:31 +0000

    syndication: do not geocode if region is '-', too
    git-svn-id: file:///Repos/zzwrap/trunk@1325 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/syndication.inc.php
+++ zzwrap/syndication.inc.php
@@ -201,7 +201,7 @@ function wrap_syndication_geocode($address) {
 	}
 	$region = isset($address['country']) ? $address['country'] : '';
 	// virtual place?
-	if ($region === '--') return [];
+	if ($region === '-' OR $region === '---') return [];
 
 	// place is optional
 	// remove parts of place name that are already found in other keys
