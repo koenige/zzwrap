737a0e9d98000833c1354d1ed0825d55ef33b2cd
Gustaf Mossakowski <gustaf@koenige.org>
2020-11-02 16:01:32 +0000

    file: allow notation as range from-to for lines to be deleted
    git-svn-id: file:///Repos/zzwrap/trunk@1340 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/file.inc.php
+++ zzwrap/file.inc.php
@@ -8,7 +8,7 @@
  * http://www.zugzwang.org/projects/zzwrap
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2017 Gustaf Mossakowski
+ * @copyright Copyright © 2017, 2020 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -29,7 +29,12 @@ function wrap_file_delete_line($file, $lines) {
 	$deleted = 0;
 	$content = file($file);
 	foreach ($lines as $line) {
-		$line = explode(',', $line);
+		if (strstr($line, '-')) {
+			$line = explode('-', $line);
+			$line = range($line[0], $line[1]);
+		} else {
+			$line = explode(',', $line);
+		}
 		foreach ($line as $no) {
 			unset($content[$no]);
 			$deleted++;
