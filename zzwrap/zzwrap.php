ca2990104f5114560bb6e2a9576111d24402f7e7
Gustaf Mossakowski <gustaf@koenige.org>
2023-01-30 09:44:05 +0000

    access: just send key 'parameters' to wrap_access_page(), rest of $zz_page is of no use, easier use of function
    git-svn-id: file:///Repos/zzwrap/trunk@1787 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/zzwrap.php
+++ zzwrap/zzwrap.php
@@ -94,7 +94,7 @@ function zzwrap() {
 	
 	if (wrap_get_setting('authentication_possible')) {
 		wrap_auth();
-		wrap_access_page($zz_page['db']);
+		wrap_access_page($zz_page['db']['parameters'] ?? '');
 	}
 	wrap_check_https($zz_page);
 
