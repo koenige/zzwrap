3f406e58c44552513d2872686a66b663422a1dcc
Gustaf Mossakowski <gustaf@koenige.org>
2023-05-06 08:25:37 +0000

    core: leave ending as for HTML output if output was changed to JSON in wrap_check_canonical()
    git-svn-id: file:///Repos/zzwrap/trunk@1899 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/core.inc.php
+++ zzwrap/core.inc.php
@@ -689,6 +689,7 @@ function wrap_check_canonical($zz_page, $page) {
 	// if database allows field 'ending', check if the URL is canonical
 	// just for HTML output!
 	if (!empty($page['content_type']) AND $page['content_type'] !== 'html'
+		AND (empty($page['content_type_original']) OR $page['content_type_original'] !== 'html')
 		AND !empty($zz_page['db']['identifier'])
 		AND substr($zz_page['db']['identifier'], -1) === '*') {
 		if (empty($page['url_ending'])) $page['url_ending'] = 'none';
