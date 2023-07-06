530103f0b5c2b1e73ecfcc12fa977e2b880aefbe
Gustaf Mossakowski <gustaf@koenige.org>
2023-07-06 07:18:27 +0000

    core/cache: move caching functions to separate file
    git-svn-id: file:///Repos/zzwrap/trunk@1969 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/zzwrap.php
+++ zzwrap/zzwrap.php
@@ -124,6 +124,7 @@ function wrap_includes() {
 	require_once __DIR__.'/errorhandling.inc.php';
 	require_once __DIR__.'/database.inc.php';
 	require_once __DIR__.'/core.inc.php';
+	require_once __DIR__.'/cache.inc.php';
 	require_once __DIR__.'/mail.inc.php';
 	require_once __DIR__.'/access.inc.php';
 	require_once __DIR__.'/language.inc.php';
