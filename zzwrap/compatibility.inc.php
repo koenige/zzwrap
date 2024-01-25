09eca21e3fbdb81ced239bd5bb3608872edca604
Gustaf Mossakowski <gustaf@koenige.org>
2024-01-25 23:31:02 +0000

    compatibility: add apache_request_headers() for PHP < 7.3
    git-svn-id: file:///Repos/zzwrap/trunk@2066 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/compatibility.inc.php
+++ zzwrap/compatibility.inc.php
@@ -58,3 +58,30 @@ if (!function_exists('mb_str_split')) {
         return $split;
     }
 }
+
+// ------------------------------------------------------------------ //
+// PHP < 7.3
+if (version_compare(PHP_VERSION, '7.3.0', '>')) return;
+// ------------------------------------------------------------------ //
+
+if( !function_exists('apache_request_headers')) {
+	function apache_request_headers() {
+		$arh = [];
+		$rx_http = '/\AHTTP_/';
+		foreach ($_SERVER as $key => $val) {
+			if (!preg_match($rx_http, $key)) continue;
+			$arh_key = preg_replace($rx_http, '', $key);
+			$rx_matches = [];
+			// restore original case, as good as possible
+			$arh_key = strtolower($arh_key);
+			$rx_matches = explode('_', $arh_key);
+			if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
+				foreach($rx_matches as $ak_key => $ak_val)
+					$rx_matches[$ak_key] = ucfirst($ak_val);
+				$arh_key = implode('-', $rx_matches);
+			}
+			$arh[$arh_key] = $val;
+		}
+		return ($arh);
+	}
+}
