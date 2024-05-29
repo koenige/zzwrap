9392e87ce0736865bde47c720590ebbcc9237d00
Gustaf Mossakowski <gustaf@koenige.org>
2024-05-29 15:27:49 +0000

    core: fix in `wrap_check_canonical()`, check both query string key and full value for ignore, not only first
    git-svn-id: file:///Repos/zzwrap/trunk@2150 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/core.inc.php
+++ zzwrap/core.inc.php
@@ -777,9 +777,12 @@ function wrap_check_canonical($zz_page, $page) {
 			if (in_array($param, $page['query_strings_redirect'])) continue;
 			if (is_array($param_value)) $param_value = http_build_query($param_value);
 			$qs_key_value = sprintf('%s=%s', $param, $param_value);
-			if (!wrap_error_ignore('qs', $qs_key_value))
-				$wrong_qs[] = $qs_key_value;
-			elseif (!wrap_error_ignore('qs', $param))
+			$ignore = false;
+			if (wrap_error_ignore('qs', $qs_key_value))
+				$ignore = true;
+			elseif (wrap_error_ignore('qs', $param))
+				$ignore = true;
+			if (!$ignore)
 				$wrong_qs[] = $qs_key_value;
 		}
 		if ($wrong_qs)
