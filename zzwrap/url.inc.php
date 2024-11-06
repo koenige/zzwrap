2dff7b210308c87340b3ffb1fb0350a413a27144
Gustaf Mossakowski <gustaf@koenige.org>
2024-11-06 16:42:55 +0000

    url: return with a 400 if Request URI does not start with a slash
    git-svn-id: file:///Repos/zzwrap/trunk@2257 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/url.inc.php
+++ zzwrap/url.inc.php
@@ -20,6 +20,12 @@ function wrap_url_prepare() {
 	global $zz_page;
 
 	// check REQUEST_URI
+	if (!str_starts_with($_SERVER['REQUEST_URI'], '/')) {
+		wrap_quit(400, wrap_text(wrap_text(
+			'Invalid Request URI: %s%s',
+			['values' => [wrap_setting('host_base'), $_SERVER['REQUEST_URI']]]
+		)));
+	}
 	wrap_url_encode();
 	wrap_url_slashes();
 	wrap_url_read();
