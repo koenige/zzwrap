f2859d819352284f4a468de2b635e9f288135bbe
Gustaf Mossakowski <gustaf@koenige.org>
2025-09-27 21:23:04 +0000

    errorhandling: fix brackets
    git-svn-id: file:///Repos/zzwrap/trunk@2420 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzwrap/errorhandling.inc.php
+++ zzwrap/errorhandling.inc.php
@@ -732,7 +732,7 @@ function wrap_error_referer_valid($non_urls = false, $local_redirects = true) {
 		// would have been redirected as well
 		if ($referer['path'] !== $zz_page['url']['full']['path']) return false;
 		// local hostnames won’t redirect from a different port, so this is a fake referer
-		if (!empty($referer['port'] AND in_array($referer['port'], [80, 443]))) return false;
+		if (!empty($referer['port']) AND in_array($referer['port'], [80, 443])) return false;
 	}
 	// referer from own domain, but invalid because should be https?
 	if (wrap_error_referer_local_https($referer, $zz_page['url']['full'])) {
