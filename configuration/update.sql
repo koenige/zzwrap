82ca5190dc3ecb2a74dcaa4a80de08c7d6f50bfe
Gustaf Mossakowski <gustaf@koenige.org>
2026-03-14 19:40:38 +0100

    update: bugfix in query with login_entry_path

--- configuration/update.sql
+++ configuration/update.sql
@@ -15,5 +15,5 @@
 /* 2023-08-04-2 */	UPDATE _settings SET setting_key = 'login_entry_path' WHERE setting_key = 'login_entryurl';
 /* 2026-03-10-1 */	DELETE FROM _settings WHERE setting_key = 'change_password_path';
 /* 2026-03-10-2 */	DELETE FROM _settings WHERE setting_key = 'jobmanager_path';
-/* 2026-03-14-1 */	UPDATE webpages SET parameters = CONCAT(IFNULL(parameters, ''), '&route=login_entry') WHERE CONCAT(identifier, IF(ending = 'none', '', ending)) = '/*_SETTING login_entry_path */';
+/* 2026-03-14-1 */	UPDATE webpages SET parameters = CONCAT(IFNULL(parameters, ''), '&route=login_entry') WHERE CONCAT(identifier, IF(ending = 'none', '', ending)) = '/*_SETTING login_entry_path _*/';
 /* 2026-03-14-2 */	DELETE FROM _settings WHERE setting_key = 'login_entry_path';
